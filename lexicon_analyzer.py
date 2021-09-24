2f63d57d1d959d3eda161f26f00f671c35ee6a0f
MathPelicer <matheuspelicer58@gmail.com>
2021-09-23 22:50:37 -0300

    first commit - files that we already had before creating this repository. Lexical analyzer being developed at this moment

---
+++ lexicon_analyzer.py
@@ -0,0 +1,174 @@
+#MEMBROS:
+#Guilherme Chagas / 22119013-5
+#Luca Milla / 22119002-8
+#Matheus Pelicer / 22119024-2
+#Thiago Soares / 22.119.044-0
+
+#S.A.D. = Sistema Automato Diferenciado/Depressivo
+
+#Comment: (:
+#Line Ender: :(
+
+#def: death
+
+#for: breath
+#while: life
+#break: choke
+
+#int: integrity [0 - 9]+
+#float: breakdown [0 - 9]+ "." [0 - 9]+
+#char: letter [a - z]+
+
+#String: "" || ''
+
+#if: what
+#elif: everwhat
+#else: ever
+
+#and: &&
+#or: ||
+
+#switch: happy
+#case: tree
+#default: accident
+
+#Operações Aritméticas
+#Soma: +
+#Subtração: -
+#Multiplicação: *
+#Divisão: /
+
+#Identificadores
+#variáveis: ([a-z])+([a-z]|[0-9]|[_])*
+#funções: ([a-z])+([a-z]|[0-9]|[_])*
+
+#Alfabetos
+#A: {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,0,1,2,3,4,5,6,7,8,9,_}
+
+#============================================================================================================================================#
+#********************************************************************************************************************************************#
+#============================================================================================================================================#
+
+#criar classe Symbol
+
+import re
+
+class Symbol:
+    def __init__(self, name, type, scope):
+        self.name = name
+        self.type = type
+        self.scope = scope
+
+#symbol atributos tipo scope (valor)
+#métodos inserção (adicionar direto no dicionário) e busca (procurar no dicionário)
+class Token(object):
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+  #representação em string da classe
+    def __str__(self):
+        return "Token({type}, {value})".format(type = self.type, value = repr(self.value))
+
+    def __repr__(self):
+        return self.__str__()
+
+class SymbolTable:
+    def __init__(self):
+        self.elements = {}
+    
+    def insert(self, symbol):
+        self.elements[str(symbol.name)] = symbol
+
+    def lookup(self, name):
+        return self.elements[str(name)]
+
+#criar classe do Analisador Léxico
+
+class LexiconAnalayzer(object):
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        self.current_char = self.text[self.pos]
+
+    def error(self):
+        raise Exception("Invalid character!")
+
+    #def advance(self):
+    #    self.pos += 1
+    #    if self.pos > len(self.text) - 1:
+    #        self.current_char = None
+    #    else:
+    #        self.current_char = self.text[self.pos]
+
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def digit(self):
+        result = ""
+        while self.current_char is not None and (self.current_char.isdigit() or self.current_char == "."):
+            result += self.current_char
+            self.advance()
+        return int(result)
+
+def token_generator(code):
+        
+    keywords = {'death', 'breath', 'life', 'choke', 'what', 'everwhat', 'ever', 'happy', 'tree', 'accident'}
+    token_specification = [
+            ('DEATH',    r'death'),       # function declaration
+            ('BREATH',   r'breath'),      # for
+            ('LIFE',     r'life'),        # while
+            ('CHOKE',    r'choke'),       # break
+            ('WHAT',     r'what'),        # if
+            ('EVERWHAT', r'everwhat'),    # elif
+            ('EVER',     r'ever'),        # else
+            ('HAPPY',    r'happy'),       # switch
+            ('TREE',     r'tree'),        # case
+            ('ACCIDENT', r'accident'),    # default
+            ('INTEGER',   r'\d+'),        # int number
+            ('FLOAT',    r'\d+(\.\d*)?'), # float number
+            ('STRING',   r'\"[a-z]+\"'),  # string always using double quotes
+            ('CHAR',     r'\'[a-z]+\''),  # char always using sigle quotes
+            ('ID',       r'[A-Za-z]+'),   # Identifiers
+            ('PLUS',     r'\+'),           # plus op +
+            ('MINUS',    r'-'),           # minus op -
+            ('MULTI',    r'\*'),           # multiplication op *
+            ('DIV',      r'/'),           # divising op /
+            ('ASSIGN',   r'='),           # Assignment operator
+            ('ENDLINE',  r'\n'),            # endline
+            #('COMMENT',  r''),            # comment
+            ('SKIP',     r'[ \t]+'),      # skip char for spaces and tabs
+            ('MISMATCH', r'\.'),           # Any other character
+        ]
+
+    tok_regex = '|'.join('(?P<%s>%s)' % pair for pair in token_specification)
+    line_num = 1
+    line_start = 0
+    print(tok_regex)
+    for mo in re.finditer(re.escape(tok_regex), code):
+        kind = mo.lastgroup
+        value = mo.group()
+        column = mo.start() - line_start
+        if kind == 'NUMBER':
+            value = float(value) if '.' in value else int(value)
+        elif kind == 'ID' and value in keywords:
+            kind = value
+        elif kind == 'NEWLINE':
+            line_start = mo.end()
+            line_num += 1
+            continue
+        elif kind == 'SKIP':
+            continue
+        elif kind == 'MISMATCH':
+            raise RuntimeError(f'{value!r} unexpected on line {line_num}')
+        yield Token(kind, value, line_num, column)
+
+statements = '''
+    thing = 9 + 10
+    more = 8 + 9
+'''
+
+for token in token_generator(statements):
+    print(token)
+            
