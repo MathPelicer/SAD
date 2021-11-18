ee23d0737fb8ed638d5c3925c29f9f8e4c39721c
MathPelicer <matheuspelicer58@gmail.com>
2021-11-18 12:21:52 -0300

    + copied class "NodeVisitor" + declaration of the vsit functions at the Interpreter_2 class

--- lexicon_analyzer.py
+++ lexicon_analyzer.py
@@ -405,9 +405,36 @@ class Interpreter(object):
 
         return 'valid'
 
+class NodeVisitor(object):
+    def visit(self, node):
+        method_name = 'visit_' + type(node).__name__
+        visitor = getattr(self, method_name, self.generic_visit)
+        return visitor(node)
+
+    def generic_visit(self, node):
+        raise Exception('No visit_{} method'.format(type(node).__name__))
+
 class Interpreter_2(object):
     def __init__(self, parser):
         self.parser = parser
+
+    def visit_StatList(self, node):
+        return None
+
+    def visit_opStatOp(self, node):
+        return None
+
+    def visit_statOp(self, node):
+        return None
+
+    def visit_CompOp(self, node):
+        return None
+
+    def visit_BinOp(self, node):
+        return None
+
+    def visit_Num(self, node):
+        return None
 
     def interpret(self):
         tree = self.parser.parse()
